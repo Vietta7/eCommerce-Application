6532a9464e99282e4d47ed3173337e9c366aa20c
Dmitry Kuchukov <kudim@inbox.ru>
2025-04-30 11:17:19 +0300

    feat:add first component

---
+++ src/components/HeadingStore/HeadingStore.tsx
@@ -0,0 +1,9 @@
+import styles from './HeadingStore.module.css';
+
+export default function HeadingStore() {
+  return (
+    <div>
+      <h1 className={styles.title}>Dino Land Store!!!</h1>
+    </div>
+  );
+}
