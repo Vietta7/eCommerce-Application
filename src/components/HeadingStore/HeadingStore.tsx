a064621d368f4919316a4c67035588e63b84bff5
kkotes <seminae99@gmail.com>
2025-05-08 20:44:53 +0300

    feat: add text input component, add global font, add packages

--- src/components/HeadingStore/HeadingStore.tsx
+++ src/components/HeadingStore/HeadingStore.tsx
@@ -1,9 +1,13 @@
+import { useState } from 'react';
+import TextInput from '../../ui-kit/TextInput/TextInput';
 import styles from './HeadingStore.module.css';
 
 export default function HeadingStore() {
+  const [value, setValue] = useState('');
   return (
     <div>
       <h1 className={styles.title}>Dino Land Store!!!</h1>
+      <TextInput value={value} onChange={setValue} />
     </div>
   );
 }
