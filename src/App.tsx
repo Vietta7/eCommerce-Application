9a5c094047727dbf59258ce501b6c39642933baa
Anna <annaspacer7@gmail.com>
2025-05-19 16:06:42 +0300

    refactor: del import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react';
-import './App.css';
 import { AccessTokenContext } from './context/AccessTokenContext';
 import AutorisationPage from './pages/AutorisationPage/AutorisationPage';
 
