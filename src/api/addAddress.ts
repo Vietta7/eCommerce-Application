66bc239bbd2f64f7e5bded34a25729fd2109312a
Dmitry Kuchukov <kudim@inbox.ru>
2025-05-30 01:33:32 +0300

    feat: create functions for address API

---
+++ src/api/addAddress.ts
@@ -0,0 +1,44 @@
+import toast from 'react-hot-toast';
+import { Address } from '../types/user/formData';
+
+interface Action {
+  action: string;
+  address?: Address;
+  addressKey?: string;
+}
+
+interface addAddressProps {
+  userToken: string;
+  version: number;
+  actions: Action[];
+}
+
+export const addAddress = async ({ userToken, version, actions }: addAddressProps) => {
+  try {
+    const response = await fetch(`https://api.europe-west1.gcp.commercetools.com/dino-land/me`, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${userToken}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        actions,
+        version,
+      }),
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      if (errorData.message) {
+        toast.error(errorData.message);
+        throw new Error(errorData.message);
+      }
+      throw new Error('Error add address');
+    } else {
+      toast.success('Add address successfull!');
+    }
+  } catch (error) {
+    console.error('Error add address:', error);
+    return null;
+  }
+};
---
+++ src/api/changeAddress.ts
@@ -0,0 +1,43 @@
+import toast from 'react-hot-toast';
+import { Address } from '../types/user/formData';
+
+export type Action =
+  | { action: 'changeAddress'; address: Address; addressId: string }
+  | { action: 'setDefaultShippingAddress'; addressId?: string }
+  | { action: 'setDefaultBillingAddress'; addressId?: string };
+
+interface changeAddressProps {
+  userToken?: string;
+  version: number;
+  actions: Action[];
+}
+
+export const changeAddress = async ({ actions, userToken, version }: changeAddressProps) => {
+  try {
+    const response = await fetch(`https://api.europe-west1.gcp.commercetools.com/dino-land/me`, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${userToken}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        actions,
+        version,
+      }),
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      if (errorData.message) {
+        toast.error(errorData.message);
+        throw new Error(errorData.message);
+      }
+      throw new Error('Error change address');
+    } else {
+      toast.success('Change successfull!');
+    }
+  } catch (error) {
+    console.error('Error change profile information:', error);
+    return null;
+  }
+};
