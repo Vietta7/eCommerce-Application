64bc96d277f0e2f29bd066bfec065aadcd45813f
Dmitry Kuchukov <kudim@inbox.ru>
2025-05-30 22:23:33 +0300

    feat: implement show password in the Input Component

--- src/components/ui/Input/Input.tsx
+++ src/components/ui/Input/Input.tsx
@@ -1,5 +1,8 @@
 import { FieldError, UseFormRegister, Path, FieldValues } from 'react-hook-form';
 import styles from './Input.module.css';
+import { EyeOffIcon } from '../../Icons/EyeOffIcon';
+import { EyeIcon } from '../../Icons/EyeIcon';
+import { useState } from 'react';
 
 interface InputProps<T extends FieldValues> {
   label: string;
@@ -26,20 +29,38 @@ export const Input = <T extends FieldValues>({
   inputValue,
   disabled,
 }: InputProps<T>) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const isPasswordField = type === 'password';
+
+  const inputType = isPasswordField ? (showPassword ? 'text' : 'password') : type;
+
   return (
     <div className={className}>
       <label className={styles.label} htmlFor={name}>
         {label}
       </label>
-      <input
-        className={`${styles.input} ${error && styles.error_input}  ${!error && inputValue && styles.success_input}`}
-        type={type}
-        placeholder={placeholder}
-        id={name}
-        autoComplete="on"
-        disabled={disabled}
-        {...register(name, { valueAsNumber })}
-      />
+      <div className={styles.input_wrapper}>
+        <input
+          className={`${styles.input} ${error && styles.error_input}  ${!error && inputValue && styles.success_input}`}
+          type={inputType}
+          placeholder={placeholder}
+          id={name}
+          autoComplete="on"
+          disabled={disabled}
+          {...register(name, { valueAsNumber })}
+        />
+
+        {isPasswordField && (
+          <button
+            type="button"
+            className={styles.toggle_button}
+            onClick={() => setShowPassword((prev: boolean) => !prev)}
+          >
+            {showPassword ? <EyeIcon /> : <EyeOffIcon />}
+          </button>
+        )}
+      </div>
       <span className={styles.error_message}>{error && error.message}</span>
     </div>
   );
