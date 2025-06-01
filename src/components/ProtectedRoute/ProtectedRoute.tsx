6aa458f2f20a0d7543bf20e04e50c1d9b3926638
Dmitry Kuchukov <kudim@inbox.ru>
2025-06-01 17:50:57 +0300

    feat: add modal into addAdress

--- src/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react';
 import { Navigate } from 'react-router-dom';
 import { getCookie } from '../../utils/getCookie';
+import { ROUTES } from '../../routes/routes';
 
 interface ProtectedRouteProps {
   children: ReactNode;
@@ -10,7 +11,7 @@ export const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
   const tokenCustomer = getCookie('access_token');
 
   if (!tokenCustomer) {
-    return <Navigate to="/login" replace />;
+    return <Navigate to={ROUTES.LOGIN} replace />;
   }
 
   return <>{children}</>;
