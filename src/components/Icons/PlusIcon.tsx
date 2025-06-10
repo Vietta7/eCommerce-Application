90ebb2a166a484d9f85d66c0113d50ab2cc29e43
Dmitry Kuchukov <kudim@inbox.ru>
2025-06-10 10:24:02 +0300

    feat: create icon components

---
+++ src/components/Icons/PlusIcon.tsx
@@ -0,0 +1,27 @@
+export const PlusIcon = () => {
+  return (
+    <svg
+      width="15.000000"
+      height="15.000000"
+      viewBox="0 0 15 15"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+    >
+      <path
+        id="Vector 110"
+        d="M0 7.5L15 7.5"
+        stroke="#00523B"
+        strokeOpacity="1.000000"
+        strokeWidth="1.500000"
+      />
+      <path
+        id="Vector 111"
+        d="M7.5 0L7.5 15"
+        stroke="#00523B"
+        strokeOpacity="1.000000"
+        strokeWidth="1.500000"
+      />
+    </svg>
+  );
+};
