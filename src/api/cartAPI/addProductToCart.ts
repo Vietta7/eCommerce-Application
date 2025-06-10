444b605c5a3c785ac7f2607e02a30c8c90d69d89
Dmitry Kuchukov <kudim@inbox.ru>
2025-06-10 10:22:17 +0300

    feat: implement functions API for work with cart

--- src/api/cartAPI/addProductToCart.ts
+++ src/api/cartAPI/addProductToCart.ts
@@ -1,22 +1,49 @@
 import toast from 'react-hot-toast';
 import { getCookie } from '../../utils/getCookie';
 
-export const getCart = async () => {
+export interface AddProductToCart {
+  cartId: string;
+  version: number;
+  productId: string;
+  quantity: number;
+  variantId: number;
+}
+
+export const addProductToCart = async ({
+  cartId,
+  version,
+  productId,
+  variantId,
+  quantity = 1,
+}: AddProductToCart) => {
   const userToken = getCookie('access_token');
+
+  const body = {
+    version,
+    actions: [
+      {
+        action: 'addLineItem',
+        productId,
+        variantId,
+        quantity,
+      },
+    ],
+  };
+
   try {
     const response = await fetch(
-      `https://api.europe-west1.gcp.commercetools.com/dino-land/me/active-cart`,
+      `https://api.europe-west1.gcp.commercetools.com/dino-land/me/carts/${cartId}`,
       {
-        method: 'GET',
+        method: 'POST',
         headers: {
           Authorization: `Bearer ${userToken}`,
           'Content-Type': 'application/json',
         },
+        body: JSON.stringify(body),
       },
     );
 
     const cart = await response.json();
-    console.log(cart);
 
     if (!response.ok) {
       if (cart.message) {
@@ -25,7 +52,7 @@ export const getCart = async () => {
       }
       throw new Error('Error get cart');
     }
-
+    toast.success('Product add to cart');
     return cart;
   } catch (error) {
     console.error('Error get cart:', error);
--- src/api/cartAPI/deleteProductFromCart.ts
+++ src/api/cartAPI/deleteProductFromCart.ts
@@ -1,18 +1,32 @@
 import toast from 'react-hot-toast';
 import { getCookie } from '../../utils/getCookie';
 
-interface CreateCartProps {
-  customerId: string;
+export interface DeleteProductToCartProps {
+  cartId: string;
+  version: number;
+  lineItemId: string;
 }
 
-export const createCart = async ({ customerId }: CreateCartProps) => {
+export const deleteProductFromCart = async ({
+  cartId,
+  version,
+  lineItemId,
+}: DeleteProductToCartProps) => {
   const userToken = getCookie('access_token');
 
-  const body = { customerId, currency: 'USD' };
+  const body = {
+    version,
+    actions: [
+      {
+        action: 'removeLineItem',
+        lineItemId,
+      },
+    ],
+  };
 
   try {
     const response = await fetch(
-      `https://api.europe-west1.gcp.commercetools.com/dino-land/me/carts`,
+      `https://api.europe-west1.gcp.commercetools.com/dino-land/me/carts/${cartId}`,
       {
         method: 'POST',
         headers: {
@@ -30,12 +44,12 @@ export const createCart = async ({ customerId }: CreateCartProps) => {
         toast.error(cart.message);
         throw new Error(cart.message);
       }
-      throw new Error('Error create cart');
+      throw new Error('Error delete from cart');
     }
-
+    toast.success('Product delete from cart');
     return cart;
   } catch (error) {
-    console.error('Error create cart:', error);
+    console.error('Error delete from cart:', error);
     return null;
   }
 };
